874c99366e9359a4b05ee8055f89c502069d3498
memorvin <alvina.tjia@yahoo.com>
2020-02-03 23:47:14 +0700

    converting stateful to stateless component

--- src/components/PictureList.jsx
+++ src/components/PictureList.jsx
@@ -1,57 +1,98 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import axios from 'axios'
 import PictureCard from './PictureCard'
 import SearchBox from './SearchBox'
 
-export default class MainContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pictures: []
-    }
-    this.onDateSearch = this.onDateSearch.bind(this)
-  }
+// export default class MainContainer extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       pictures: []
+//     }
+//     this.onDateSearch = this.onDateSearch.bind(this)
+//   }
 
-  componentDidMount() {
+//   componentDidMount() {
+//     axios.get(`${process.env.REACT_APP_API_URL}&count=15`)
+//       .then(({ data }) => {
+//         this.setState({ pictures: data })
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+//   }
+
+//   componentWillUnmount() {
+//     this.setState({
+//       pictures: []
+//     })
+//   }
+
+//   onDateSearch(value) {
+//     axios.get(`${process.env.REACT_APP_API_URL}&start_date=${value.startYear}-${value.startMonth}-${value.startDate}&end_date=${value.endYear}-${value.endMonth}-${value.endDate}`)
+//       .then(({ data }) => {
+//         this.setState({ pictures: data })
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+//   }
+
+//   PicturesList() {
+//     return this.state.pictures.map(picture => {
+//       return <PictureCard picture={picture} key={picture.date} />
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <SearchBox onSearch={this.onDateSearch} />
+//         <div className="flex flex-wrap text-center ml-40">
+//           {this.PicturesList()}
+//         </div>
+//       </>
+//     )
+//   }
+// }
+
+export default function MainContainer() {
+  // this.onDateSearch = this.onDateSearch.bind(this)
+
+  const [pictures, setPictures] = useState([])
+
+  useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_URL}&count=15`)
       .then(({ data }) => {
-        this.setState({ pictures: data })
+        setPictures(data)
       })
       .catch(err => {
         console.log(err)
       })
-  }
+  }, [])
 
-  componentWillUnmount() {
-    this.setState({
-      pictures: []
-    })
-  }
-
-  onDateSearch(value) {
+  const onDateSearch = ((value) => {
     axios.get(`${process.env.REACT_APP_API_URL}&start_date=${value.startYear}-${value.startMonth}-${value.startDate}&end_date=${value.endYear}-${value.endMonth}-${value.endDate}`)
       .then(({ data }) => {
-        this.setState({ pictures: data })
+        setPictures(data)
       })
       .catch(err => {
         console.log(err)
       })
-  }
+  })
 
-  PicturesList() {
-    return this.state.pictures.map(picture => {
+  const PicturesList = (() => {
+    return pictures.map(picture => {
       return <PictureCard picture={picture} key={picture.date} />
     })
-  }
+  })
 
-  render() {
-    return (
-      <>
-        <SearchBox onSearch={this.onDateSearch} />
-        <div className="flex flex-wrap text-center ml-40">
-          {this.PicturesList()}
-        </div>
-      </>
-    )
-  }
+  return (
+    <>
+      <SearchBox onSearch={onDateSearch} />
+      <div className="flex flex-wrap text-center ml-40">
+        {PicturesList()}
+      </div>
+    </>
+  )
 }
